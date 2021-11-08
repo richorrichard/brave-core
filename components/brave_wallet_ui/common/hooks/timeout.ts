43a799827013c09ee4951896391d9e7197060ae6
Douglas Daniel <douglashdaniel@gmail.com>
2021-11-08 16:50:41 -0700

    Improved Wallet Timeout Hook and Performance

--- components/brave_wallet_ui/common/hooks/timeout.ts
+++ components/brave_wallet_ui/common/hooks/timeout.ts
@@ -9,40 +9,50 @@ export default function useTimeout (
   notifyUserInteraction: () => void,
   isWalletLocked: boolean
 ) {
-  const [lastCheckedTime, setLastCheckedTime] = React.useState<number>()
-  const [currentMousePosition, setCurrentMousePosition] = React.useState({ x: 0, y: 0 })
-  const [lastMousePosition, setLastMousePosition] = React.useState({ x: 0, y: 0 })
-  React.useMemo(() => {
-    const { x, y } = currentMousePosition
-    // Checks to see if the mouse moved everytime lastCheckTime is updated
-    if (lastMousePosition.x !== x || lastMousePosition.y !== y) {
-      setLastMousePosition({ x, y })
-      // If mouse has moved will notify keyring here.
-      notifyUserInteraction()
+  const hasInteractedSinceInterval = React.useRef<boolean>(false)
+  const onMouseMove = () => {
+    if (!hasInteractedSinceInterval.current) {
+      hasInteractedSinceInterval.current = true
     }
-  }, [lastCheckedTime])
+  }
+
+  const onKeyDown = () => {
+    if (!hasInteractedSinceInterval.current) {
+      hasInteractedSinceInterval.current = true
+    }
+  }
+
+  const onVisibilityChange = () => {
+    if (document.visibilityState === 'visible' && !hasInteractedSinceInterval.current) {
+      hasInteractedSinceInterval.current = true
+    }
+  }
 
   React.useEffect(() => {
-    // Starts tracking mouse move events when wallet is unlocked
+    const removeEventListeners = () => {
+      window.removeEventListener('mousemove', onMouseMove)
+      window.removeEventListener('keydown', onKeyDown)
+      document.removeEventListener('visibilitychange', onVisibilityChange)
+    }
+
     if (!isWalletLocked) {
-      const mousePosition = (event: MouseEvent) => setCurrentMousePosition({ x: event.clientX, y: event.clientY })
-      window.addEventListener('mousemove', mousePosition)
-      return () => window.removeEventListener('mousemove', mousePosition)
+      window.addEventListener('mousemove', onMouseMove)
+      window.addEventListener('keydown', onKeyDown)
+      document.addEventListener('visibilitychange', onVisibilityChange)
     }
-    return
-  }, [isWalletLocked])
+    return removeEventListeners
+  }, [isWalletLocked, hasInteractedSinceInterval])
 
   React.useEffect(() => {
-    // If wallet is unlocked, will start interval
-    // and setLastCheckedTime every 50 seconds to trigger
-    // a check to see if the mouse moved.
     if (!isWalletLocked) {
       const interval = setInterval(() => {
-        const date = Date.now()
-        setLastCheckedTime(date)
+        if (hasInteractedSinceInterval.current) {
+          hasInteractedSinceInterval.current = false
+          notifyUserInteraction()
+        }
       }, 50000)
       return () => clearInterval(interval)
     }
     return
-  }, [isWalletLocked])
+  }, [isWalletLocked, hasInteractedSinceInterval])
 }
